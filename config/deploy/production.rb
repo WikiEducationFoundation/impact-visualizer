2856ad1ba1cff9fb23ad69f5c80c35f61c2c5d1a
Matthew Fordham <hello@matthewfordham.com>
2023-09-28 12:39:14 -0700

    Yarn tweak for deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 set :branch, "production"
 set :linked_files, %w{config/master.key config/credentials/production.key}
+set :default_env, { path: '$PATH:~/.nvm/versions/node/v18.17.1/bin' }
 server "172.232.172.116", user: "deploy", roles: %w{app db web}
