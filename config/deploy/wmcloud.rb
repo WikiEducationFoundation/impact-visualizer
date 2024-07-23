1c49f78ef17c0c64bd9f73fae0a6162ef6a1b909
Matthew Fordham <hello@matthewfordham.com>
2024-07-23 12:23:12 -0700

    Deploy tweaks for wmcloud

--- config/deploy/wmcloud.rb
+++ config/deploy/wmcloud.rb
@@ -1,5 +1,5 @@
 set :branch, "production"
 set :linked_files, %w{config/master.key config/credentials/wmcloud.key}
 set :default_env, { path: '$PATH:~/.nvm/versions/node/v20.15.1/bin' }
-set :stage, :production
+set :stage, :wmcloud
 server "impact-visualizer", user: "mattfordham", roles: %w{app db web}
