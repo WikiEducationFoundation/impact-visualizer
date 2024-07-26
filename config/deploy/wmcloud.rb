5766a332403e6700cb0699429d40a5071ce817c9
Matthew Fordham <hello@matthewfordham.com>
2024-07-26 11:59:08 -0700

    Capistrano / Sidekiq tweaks for wmcloud

--- config/deploy/wmcloud.rb
+++ config/deploy/wmcloud.rb
@@ -2,4 +2,5 @@ set :branch, "production"
 set :linked_files, %w{config/master.key config/credentials/wmcloud.key}
 set :default_env, { path: '$PATH:~/.nvm/versions/node/v20.15.1/bin' }
 set :stage, :wmcloud
+set :sidekiq_service_unit_user, :system
 server "impact-visualizer", user: "mattfordham", roles: %w{app db web}
