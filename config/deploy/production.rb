387e4d37afcf3c9686ed12bfbbfd7252794d0fa9
Matthew Fordham <hello@matthewfordham.com>
2024-11-07 14:26:39 -0800

    Deploy tweaks

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,5 @@ set :branch, "production"
 set :linked_files, %w{config/master.key config/credentials/production.key}
 set :default_env, { path: '$PATH:~/.nvm/versions/node/v18.17.1/bin' }
 set :stage, :production
+set :sidekiq_service_unit_user, :system
 server "172.232.172.116", user: "deploy", roles: %w{app db web}
