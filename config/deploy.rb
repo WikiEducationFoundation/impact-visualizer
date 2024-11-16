9eb78a99b8f7b1d14ed337400f4d7eb466ec6536
Matthew Fordham <hello@matthewfordham.com>
2024-11-15 16:52:35 -0800

    Sidekiq config fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.3"
+lock "~> 3.19.2"
 
 set :application, "impact-visualizer"
 set :repo_url, "git@github.com:WikiEducationFoundation/impact-visualizer.git"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :branch, "production"
-set :linked_files, %w{config/master.key config/credentials/production.key}
+set :linked_files, %w{config/sidekiq.yml config/master.key config/credentials/production.key}
 append :linked_dirs, "log", "tmp/cache", "tmp/pids", "tmp/storage"
 
 set :default_env, { path: '$PATH:~/.nvm/versions/node/v18.17.1/bin' }
@@ -11,5 +11,5 @@ set :sidekiq_roles, %w{sidekiq app}
 set :console_role, :app
 # set :console_role, :sidekiq
 
-server "172.232.172.116", user: "deploy", roles: %w{app db web}, sidekiq_config_files: %w[sidekiq-import.yml]
-server "172.234.250.245", user: "deploy", roles: %w{sidekiq}, sidekiq_config_files: %w[sidekiq-timepoints.yml]
+server "172.232.172.116", user: "deploy", roles: %w{app db web}
+server "172.234.250.245", user: "deploy", roles: %w{sidekiq}
