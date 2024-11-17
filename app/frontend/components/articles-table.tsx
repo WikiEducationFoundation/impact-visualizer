b0eb6240a15add1773423f6adaba224bb810b05f
Amine Hassou <japnoo@gmail.com>
2024-11-17 02:47:00 +0100

    Added "no articles found" for articles table in case of no articles

--- app/frontend/components/articles-table.tsx
+++ app/frontend/components/articles-table.tsx
@@ -9,28 +9,37 @@ export default function ArticlesTable({
   articles: string[];
   filename: string;
 }) {
+  const hasArticles = articles.length > 0;
   return (
     <table>
       <thead>
         <tr>
           <th>
             Article
-            <CSVButton
-              articles={articles}
-              csvConvert={convertArticlesToCSV}
-              filename={filename}
-            />
+            {hasArticles ? (
+              <CSVButton
+                articles={articles}
+                csvConvert={convertArticlesToCSV}
+                filename={filename}
+              />
+            ) : null}
           </th>
         </tr>
       </thead>
       <tbody>
-        {articles?.map((article, index) => (
-          <tr key={index}>
-            <td>
-              <div>{article}</div>
-            </td>
+        {hasArticles ? (
+          articles?.map((article, index) => (
+            <tr key={index}>
+              <td>
+                <div>{article}</div>
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td>No articles found</td>
           </tr>
-        ))}
+        )}
       </tbody>
     </table>
   );
