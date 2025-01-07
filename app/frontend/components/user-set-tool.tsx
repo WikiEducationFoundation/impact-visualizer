1d092e40cfa60fbaeb96ffe96e6667a7260262c0
Amine Hassou <japnoo@gmail.com>
2025-01-07 23:41:02 +0100

    Fully implemented user set tool

--- app/frontend/components/user-set-tool.tsx
+++ app/frontend/components/user-set-tool.tsx
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 import Select from "react-select";
 import toast from "react-hot-toast";
+import { UserSetResponse } from "../types/search-tool.type";
+import LoadingOval from "./loading-oval.component";
+import ArticlesTable from "./articles-table";
 
 const userSetOptions = [
   { value: "", label: "-- Choose an option --" },
-  { value: "wiki-student-program", label: "Wikipedia Student Program" },
-  { value: "scholars-and-scientists-program", label: "Scholars & Scientists" },
+  { value: "classroom_program", label: "Wikipedia Student Program" },
+  { value: "fellows_cohort", label: "Scholars & Scientists" },
 ];
 
 const userSetTypeOptions = [
   { value: "", label: "-- Choose an option --" },
   { value: "students", label: "Students" },
-  { value: "instructors", label: "Instructors" },
-  { value: "students and instructors", label: "Students and Instructors" },
+  { value: "students_and_instructors", label: "Students and Instructors" },
 ];
 
 export default function UserSetTool() {
   const [selectedUserSet, setSelectedUserSet] = useState<string>("");
   const [selectedUserSetTypes, setSelectedUserSetTypes] = useState<string>("");
+  const [queriedData, setQueriedData] = useState<UserSetResponse>();
+  const [queriedUsers, setQueriedUsers] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (event: React.FormEvent) => {
@@ -35,8 +39,40 @@ export default function UserSetTool() {
       setIsLoading(false);
       return;
     }
+    if (selectedUserSet === "wiki-student-program") {
+    }
+    try {
+      const response = await fetch(
+        `http://localhost:3000/courses/${[
+          selectedUserSet,
+          selectedUserSetTypes,
+        ].join("_")}.json`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data: UserSetResponse = await response.json();
+      setQueriedData(data);
 
-    setIsLoading(false);
+      const usernames: string[] = [];
+
+      data.courses.forEach((course) => {
+        course.students.forEach((student) => {
+          usernames.push(student.username);
+        });
+        if (course.instructors) {
+          course.instructors.forEach((instructor) => {
+            usernames.push(instructor.username);
+          });
+        }
+      });
+      setQueriedUsers(usernames);
+    } catch (error) {
+      console.error("Fetch error:", error);
+      toast("There was an issue fetching the data.");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -74,6 +110,26 @@ export default function UserSetTool() {
           Run Query
         </button>
       </form>
+      {isLoading ? (
+        <div className="OvalContainer">
+          <LoadingOval visible={isLoading} height="120" width="120" />
+        </div>
+      ) : (
+        <div
+          className="TablesContainer"
+          style={{ display: "flex", gap: "20px" }}
+        >
+          {queriedData && (
+            <ArticlesTable
+              articles={queriedUsers}
+              filename={`${selectedUserSet.replace(
+                "_",
+                "-"
+              )}_${selectedUserSetTypes.replace("_", "-")}.csv`}
+            />
+          )}
+        </div>
+      )}
     </div>
   );
 }
