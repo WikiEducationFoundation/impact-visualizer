0b9b137279af14050a2156f3482044771582f69e
Sage Ross <ragesoss@gmail.com>
2025-05-05 08:35:21 -0700

    Update deploy config for wmcloud
    I added a sidekiq config on the server and deployed with this, and now the import queue is working.

--- config/deploy/wmcloud.rb
+++ config/deploy/wmcloud.rb
@@ -1,6 +1,8 @@
 set :branch, "production"
-set :linked_files, %w{config/master.key config/credentials/wmcloud.key}
+set :linked_files, %w{config/master.key config/credentials/wmcloud.key config/sidekiq.yml}
 set :default_env, { path: '$PATH:~/.nvm/versions/node/v20.15.1/bin' }
 set :stage, :wmcloud
 set :sidekiq_service_unit_user, :system
-server "impact-visualizer", user: "mattfordham", roles: %w{app db web}
+set :sidekiq_roles, %w{sidekiq app}
+
+server "impact-visualizer", user: "ragesoss", roles: %w{app db web sidekiq}
